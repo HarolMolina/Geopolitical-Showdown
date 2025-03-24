56d57e2bb8c3ba0569d81297564cc88ec7a9c92e
HarolMolina <entregasenbici2002@gmail.com>
2025-03-23 20:22:26 -0400

    Comente cada seccion del codigo

--- javascript.js
+++ javascript.js
@@ -1,80 +1,80 @@
+// eleccion aleatoria de la computadora
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
   if (choice === 0) {
-    return 'piedra';
+    return "piedra";
   } else if (choice === 1) {
-    return 'papel';
+    return "papel";
   } else {
-    return 'tijera';
+    return "tijera";
   }
 }
-// let ComputerChoice = getComputerChoice(3);
-// console.log(ComputerChoice);
+
+// eleccion del jugador
 
 function getHumanChoice() {
   let humanInput = prompt(
-    'Por favor, elije piedra, papel tijera',
+    "Por favor, elije piedra, papel tijera"
   ).toLowerCase();
 
-  if (humanInput === 'piedra') {
-    return 'piedra';
-  } else if (humanInput === 'papel') {
-    return 'papel';
-  } else if (humanInput === 'tijera') {
-    return 'tijera';
+  if (humanInput === "piedra") {
+    return "piedra";
+  } else if (humanInput === "papel") {
+    return "papel";
+  } else if (humanInput === "tijera") {
+    return "tijera";
   } else {
-    return console.log('error, ingrese valor correcto');
+    return console.log("error, ingrese valor correcto");
   }
 }
-// let humanInput = getHumanChoice();
-// console.log(humanInput);
-
+// jugar una ronda
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    return 'Es un empate';
+    return "Es un empate";
   }
-  if (humanChoice === 'piedra') {
-    if (computerChoice === 'tijera') {
+  if (humanChoice === "piedra") {
+    if (computerChoice === "tijera") {
       humanScore++;
-      return 'Ganaste, Piedra rompe a tijeras';
+      return "Ganaste, Piedra rompe a tijeras";
     } else {
       computerScore++;
-      return 'Perdiste, Papel Envuelve a Piedra';
+      return "Perdiste, Papel Envuelve a Piedra";
     }
   }
-  if (humanChoice === 'tijera') {
-    if (computerChoice === 'papel') {
+  if (humanChoice === "tijera") {
+    if (computerChoice === "papel") {
       humanScore++;
-      return 'Ganaste, Tijera Corta a papel';
+      return "Ganaste, Tijera Corta a papel";
     } else {
       computerScore++;
-      return 'Perdiste, Piedra rompe a tijeras';
+      return "Perdiste, Piedra rompe a tijeras";
     }
   }
-  if (humanChoice === 'papel') {
-    if (computerChoice === 'piedra') {
+  if (humanChoice === "papel") {
+    if (computerChoice === "piedra") {
       humanScore++;
-      return 'Ganaste, papel envuelve a piedra';
+      return "Ganaste, papel envuelve a piedra";
     } else {
       computerScore++;
-      return 'Perdiste, tijera corta a papel';
+      return "Perdiste, tijera corta a papel";
     }
   }
 }
 
-function playGame() {}
-
+// variables de puntuacion por rondas
 let humanScore = 0;
 let computerScore = 0;
 
+// llamadas a las funciones de eleccion
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-
+// llamada a la funcion para jugar una ronda
 const result = playRound(humanSelection, computerSelection);
 
-console.log('Tu eleccion;', humanSelection);
-console.log('elecciond de la computadora;', computerSelection);
+// todos los console.log
+console.log("Tu eleccion;", humanSelection);
+console.log("elecciond de la computadora;", computerSelection);
 console.log(result);
-console.log('usuario', humanScore);
-console.log('computadora', computerScore);
+console.log("usuario", humanScore);
+console.log("computadora", computerScore);
