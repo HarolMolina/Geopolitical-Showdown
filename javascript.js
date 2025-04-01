467db4bf5225cd20f7fe04477d8e20c9b487631e
HarolMolina <entregasenbici2002@gmail.com>
2025-04-01 06:45:33 -0400

    logica del proyecto terminada (por ahora)
    se anadio puntuacion de los jugadores, los console logs para ver dicha puntuacion, algunas funciones que complementan y el bucle for que juega 5 rondas

--- javascript.js
+++ javascript.js
@@ -1,73 +1,90 @@
-// jugar 5 rondas
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "piedra";
+  } else if (choice === 1) {
+    return "papel";
+  } else {
+    return "tijera";
+  }
+}
+
+function getHumanChoice() {
+  let humanInput = prompt(
+    "Por favor, elije piedra, papel tijera"
+  ).toLowerCase();
+
+  if (humanInput === "piedra") {
+    return "piedra";
+  } else if (humanInput === "papel") {
+    return "papel";
+  } else if (humanInput === "tijera") {
+    return "tijera";
+  } else {
+    return console.log("error, ingrese valor correcto");
+  }
+}
+let humanscore = 0;
+let computerScore = 0;
+
+for (let i = 1; i <= 5; i++) {
+  checkScore();
+}
+
+function checkScore() {
+  if (computerScore <= 5 && humanscore <= 5) {
+    Playgame();
+  } else {
+  }
+}
+
 function Playgame() {
-  // eleccion aleatoria de la computadora
-  function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-
-    if (choice === 0) {
-      return "piedra";
-    } else if (choice === 1) {
-      return "papel";
-    } else {
-      return "tijera";
-    }
-  }
-
-  // eleccion del jugador
-  function getHumanChoice() {
-    let humanInput = prompt(
-      "Por favor, elije piedra, papel tijera"
-    ).toLowerCase();
-
-    if (humanInput === "piedra") {
-      return "piedra";
-    } else if (humanInput === "papel") {
-      return "papel";
-    } else if (humanInput === "tijera") {
-      return "tijera";
-    } else {
-      return console.log("error, ingrese valor correcto");
-    }
-  }
-
-  // llamadas a las funciones de eleccion
   let humanSelection = getHumanChoice();
   let computerSelection = getComputerChoice();
 
-  console.log("Tu eleccion;", humanSelection);
   console.log("eleccion de la computadora;", computerSelection);
 
-  // llamada a la funcion para jugar una ronda
   let result = playRound(humanSelection, computerSelection);
   console.log(result);
 
-  // jugar una ronda
   function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
+      humanscore++;
+      computerScore++;
       return "Es un empate";
     }
     if (humanChoice === "piedra") {
       if (computerChoice === "tijera") {
+        humanscore++;
         return "Ganaste, Piedra rompe a tijeras";
       } else {
+        computerScore++;
         return "Perdiste, Papel Envuelve a Piedra";
       }
     }
     if (humanChoice === "tijera") {
-      if (computerChoice === "papel") return "Ganaste, Tijera Corta a papel";
-    } else {
-      return "Perdiste, Piedra rompe a tijeras";
+      if (computerChoice === "papel") {
+        humanscore++;
+        return "Ganaste, Tijera Corta a papel";
+      } else {
+        computerScore++;
+        return "Perdiste, Piedra rompe a tijeras";
+      }
     }
     if (humanChoice === "papel") {
       if (computerChoice === "piedra") {
+        humanscore++;
         return "Ganaste, papel envuelve a piedra";
       } else {
+        computerScore++;
         return "Perdiste, tijera corta a papel";
       }
     }
   }
-}
-
-for (let i = 1; i <= 5; i++) {
-  Playgame();
+  console.log(
+    "la puntuacion es Usuario " +
+      humanscore +
+      " // computadora " +
+      computerScore
+  );
 }
