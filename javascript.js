9d6708a7c12caa74b66f99802cf5245bef41d49a
HarolMolina <entregasenbici2002@gmail.com>
2025-05-05 13:55:14 -0400

    se cambiaron los mensajes de victoria,se puso una funcion para reiniciar el juego,

--- javascript.js
+++ javascript.js
@@ -18,33 +18,35 @@ botones.forEach((button) => {
 
     function playRound(humanChoice, computerChoice) {
       if (humanChoice === computerChoice) {
-        return `${humanChoice} empata con ${computerChoice}`;
+        computerScore++;
+        humanscore++;
+        return "entre bomberos no nos pisamos la manguera";
       }
       if (humanChoice === "piedra") {
         if (computerChoice === "tijera") {
           humanscore++;
-          return "Ganaste, Piedra rompe a tijeras";
+          return "Rusia le tira un Kinzhal al chinito....";
         } else {
           computerScore++;
-          return "Perdiste, Papel Envuelve a Piedra";
+          return "Trump te mete 400% de aranceles";
         }
       }
       if (humanChoice === "tijera") {
         if (computerChoice === "papel") {
           humanscore++;
-          return "Ganaste, Tijera Corta a papel";
+          return "Se acabo el made in China y invaden taiwan";
         } else {
           computerScore++;
-          return "Perdiste, Piedra rompe a tijeras";
+          return "Putin asere, dejame ir al desfile 'en chino'";
         }
       }
       if (humanChoice === "papel") {
         if (computerChoice === "piedra") {
           humanscore++;
-          return "Ganaste, papel envuelve a piedra";
+          return "5000 millones pa ucrania como te cae";
         } else {
           computerScore++;
-          return "Perdiste, tijera corta a papel";
+          return "Wa shing tong es la nueva capital de eeuu";
         }
       }
     }
@@ -55,8 +57,10 @@ botones.forEach((button) => {
 
     if (humanscore == 5) {
       alert("Ganaste!!! Ahora tu dominas el mundo");
+      reiniciar();
     } else if (computerScore == 5) {
       alert("Perdiste!!! Bajate el Pantalon y ponte en 4");
+      reiniciar();
     }
   }
 });
@@ -72,8 +76,22 @@ function getComputerChoice() {
     pictureOnDisplay.appendChild(img);
     return "piedra";
   } else if (choice === 1) {
+    // maquina mostrar  a trump
+    let pictureOnDisplay = document.querySelector(".computer-choice");
+    let img = document.createElement("img");
+    img.src = "/Images/trump gana .webp";
+    pictureOnDisplay.textContent = "";
+    pictureOnDisplay.appendChild(img);
+
     return "papel";
   } else {
+    // maquina mostrar  a xijinping
+    let pictureOnDisplay = document.querySelector(".computer-choice");
+    let img = document.createElement("img");
+    img.src = "/Images/avocado.jpg";
+    pictureOnDisplay.textContent = "";
+    pictureOnDisplay.appendChild(img);
+
     return "tijera";
   }
 }
@@ -90,19 +108,18 @@ function getHumanChoice(button) {
     pictureOnDisplay.appendChild(img);
     return "piedra";
   } else if (humanInput === "papel") {
-    // humano mostrar  a xijinping
-    let pictureOnDisplay = document.querySelector(".human-choice");
-    let img = document.createElement("img");
-    img.src = "/Images/avocado.jpg";
-    pictureOnDisplay.textContent = "";
-    pictureOnDisplay.appendChild(img);
-
-    return "papel";
-  } else if (humanInput === "tijera") {
     // humano mostrar  a trump
     let pictureOnDisplay = document.querySelector(".human-choice");
     let img = document.createElement("img");
     img.src = "/Images/trump gana .webp";
+    pictureOnDisplay.textContent = "";
+    pictureOnDisplay.appendChild(img);
+    return "papel";
+  } else if (humanInput === "tijera") {
+    // humano mostrar  a xijinping
+    let pictureOnDisplay = document.querySelector(".human-choice");
+    let img = document.createElement("img");
+    img.src = "/Images/avocado.jpg";
     pictureOnDisplay.textContent = "";
     pictureOnDisplay.appendChild(img);
     return "tijera";
@@ -112,3 +129,14 @@ function getHumanChoice(button) {
 }
 let humanscore = 0;
 let computerScore = 0;
+
+// reiniciar juego
+function reiniciar() {
+  humanscore = 0;
+  computerScore = 0;
+
+  welcome.textContent = "Choose your weapon";
+  score.textContent = "Score";
+  choice.textContent = "❔";
+  computerChi.textContent = "❔";
+}
